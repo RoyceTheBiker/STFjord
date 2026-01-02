772d0a9a0c1f5fc119b3b5a3da1d3a40e45ed84b
Royce Souther <osgnuru@gmail.com>
2026-01-02 12:51:06 -0700

    Fixing lint errors

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = ">= 1.14.3"
+      version = "~> 2.0"
     }
     template = "2.2.0"
   }
