383d6e11847db689f8a23c1deac10f0c81101d8c
Royce Souther <osgnuru@gmail.com>
2026-01-02 14:30:08 -0700

    Unused variables
    These variables arn't used by Terraform, but they are used by payload.

---
+++ output.tf
@@ -0,0 +1,36 @@
+
+output "COMMON_NAME" {
+  value = var.COMMON_NAME
+}
+
+output "COUNTRY" {
+  value = var.COUNTRY
+}
+
+output "ENVIRONMENT" {
+  value = var.ENVIRONMENT
+}
+
+output "EMAIL_ACCOUNTS" {
+  value = var.EMAIL_ACCOUNTS
+}
+
+output "LOCATION" {
+  value = var.LOCATION
+}
+
+output "ORGANIZATION" {
+  value = var.ORGANIZATION
+}
+
+output "ORG_UNIT" {
+  value = var.ORG_UNIT
+}
+
+output "STATE" {
+  value = var.STATE
+}
+
+output "TIME_ZONE" {
+  value = var.TIME_ZONE
+}
